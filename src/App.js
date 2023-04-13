173229207f5b9e1e402c5b314ada551153147da7
amol210 <114400251+amol210@users.noreply.github.com>
2023-04-13 10:11:55 +0530

    src
    src

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Routing from "./Components/Routing"
+import Navbar from "./Components/Navbar"
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <Routing />
+    
+    </div>
+  );
+}
+
+export default App;
